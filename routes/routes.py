4ad9f581362b8723c0cae465aef4b84cc6141e2d
KJBig <kjgi73k@gmail.com>
2023-12-18 19:34:45 +0900

    route 수정

--- routes/routes.py
+++ routes/routes.py
@@ -1,17 +1,17 @@
 from fastapi import APIRouter
 from dto.commentReqDto import CommentReqDto
 from dto.itemImgReqDto import ItemImgReqDto
-from kobert import predict
-from yolov8 import detect_color
+import kobert
+import yolov8
 
 api = APIRouter()
 
 @api.post("/check-malicious-comment")
 def checkMaliciousComment(commentReqDto : CommentReqDto):
     # TODO : Call cleanBot Metho
-    return predict(commentReqDto.comment)
+    return kobert.predict(commentReqDto.comment)
 
 @api.post("/check-item-color")
 def checkMaliciousComment(itemImgReqDto : ItemImgReqDto):
     # TODO : Call cleanBot Method
-    return detect_color(itemImgReqDto.itemImgUrl)
+    return yolov8.detect_color(itemImgReqDto.itemImgUrl)
